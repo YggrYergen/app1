a65e6f959659f5ea8a15eba37d836be0b9a93493
Tomas Gemes Salinas <tomasgemes@gmail.com>
2021-03-28 20:01:07 -0300

    variable names updated, gub fiix

--- main.c
+++ main.c
@@ -5,20 +5,16 @@ int main(int argc, char** argv) {
   int numb_rows;
   // inicializa el puntero al archivo, r de read mode
   FILE* fp = fopen(archivo_csv, "r");
-  FILE* nr = fopen(archivo_csv, "r");
-  if (!fp) {  // es lo mismo que input==NULL
-    printf("Error\n");
-    return 0;
-  }
-  count_rows(&numb_rows, nr);
-  fclose(nr);
+
+  count_rows(&numb_rows, fp);
 
   libros values[numb_rows];
 
   leer_csv(fp, values);
+
   fclose(fp);
 
-  //printvalues(values, numb_rows);
+  
 
   return 0;
 }
