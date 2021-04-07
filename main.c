9577e624e8fb5e6da253a6a23e2890ead135ae34
Rodrigo Rojas <rod.rojas@alumnos.uai.cl>
2021-04-07 16:26:57 -0400

    ahora si, se me olvido el main

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
 #include "header.h"
 
+int j = 0;
+
 int main(int argc, char** argv) {
   char* archivo_csv = argv[1];
-  int numb_rows;
-  // inicializa el puntero al archivo, r de read mode
-  FILE* fp = fopen(archivo_csv, "r");
-  FILE* nr = fopen(archivo_csv, "r");
-  if (!fp) {  // es lo mismo que input==NULL
+  FILE* notas = fopen(archivo_csv, "r");
+  if (!notas) {  // es lo mismo que input==NULL
     printf("Error, ingrese el archico .cvs correctamente.\n");
     return 0;
   }
 
-  count_rows(&numb_rows, nr);
-  fclose(nr);
+  for (char c = getc(notas); c != EOF; c = getc(notas)) {
+    if (c == '\n') {
+      ++j;
+    }
+  }
 
-  libros biblioteca[numb_rows];
+  libros biblioteca[j + 1];
+  nuevos_datos NuevosDatos[100];
+  rewind(notas);
 
-  leer_csv(fp, biblioteca);
+  leer_csv(notas, biblioteca);
+  fclose(notas);
 
-  show_menu(biblioteca, numb_rows);
-
-  fclose(fp);
-
-  // printvalues(biblioteca, numb_rows);
-
-  return 0;
+  llenarSedes(biblioteca, NuevosDatos, j);
+  llenarPisos(biblioteca, NuevosDatos, j);
+  llenarSecciones(biblioteca, NuevosDatos, j);
+  // show_menu(biblioteca, NuevosDatos, archivo_csv, j);
 }
